777389689add470749311087658373c8ef948294
fadl <fadlabiola21@gmail.com>
2022-09-13 23:45:53 -0700

    prints absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - value absolute
+ * @n: the int to print
+ * return: always 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}

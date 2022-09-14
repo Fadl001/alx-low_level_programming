254e3bfa7068108effab54f8dee70934bdbeba47
fadl <fadlabiola21@gmail.com>
2022-09-14 00:02:21 -0700

    computes the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,18 +4,23 @@
  * @n: the int to print
  * return: always 0
  */
+
 int _abs(int n)
 {
+
 	if (n < 0)
 	{
 		return (n * (-1));
 	}
+
 	else if (n == 0)
 	{
 		return (0);
 	}
+
 	else
 	{
 		return (n);
 	}
+
 }
